6b3b5d5bbd280e3906b4b513b25203f5917aaa17
Andrei Kazakou <andrei.k@geomotiv.com>
2019-06-21 15:56:02 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Stage "base": build main docker environment for any node.js application
+FROM node:12-alpine as base
+
+# Install your global runtime dependencies here that are required for service to operate
+# Example: RUN apk add --update g++ make
+
+WORKDIR /home/node/app/
+
+# Stage "build": Install your build dependencies here (e.g those that are required only at a build time when doing npm install)
+FROM base AS build
+# Example: RUN apk add --update python python-dev
+
+# Stage "dependencies": Build node_modules and put it into stage docker layer cache
+FROM build AS dependencies
+COPY package.json package-lock.json payload.xml.sample /home/node/app/
+RUN npm ci
+
+# Stage "release": Copy node_modules and from docker layer cache and update application source code
+FROM base AS release
+COPY --from=dependencies /home/node/app/node_modules ./node_modules
+COPY --from=dependencies /home/node/app/payload.xml.sample ./payload.xml
+COPY . /home/node/app/
+COPY ./*.xml* /home/node/app/*
+
+ENTRYPOINT ["node", "/home/node/app/src/index.js"]
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const cluster = require('cluster');
+
+const iterationCount = Number(process.env.ITERATION_COUNT);
+const threadCount = Number(process.env.THREAD_COUNT);
+
+if (cluster.isMaster) {
+    const startTime = new Date();
+
+    console.info(`Started at ${startTime.toISOString()}`);
+    console.info(`Each thread will run ${iterationCount} iterations for test...`);
+    console.info(`Starting ${threadCount} threads for test...`);
+
+    let workersCount = 0;
+    for (let i = 0; i < threadCount; i++) {
+        workersCount++;
+        cluster.fork();
+    }
+
+    console.info('Waiting for all threads to finish...');
+    cluster.on('exit', (worker, code, signal) => {
+        workersCount--;
+        if (workersCount <= 0) {
+            const endTime = new Date();
+
+            console.info(`Completed in ${(endTime.getTime() - startTime.getTime()) / 1000} seconds.`);
+            console.info(`Finished at ${endTime.toISOString()}.`);
+
+            process.exit(0);
+        }
+    });
+} else {
+    const fs = require('fs');
+    const xmlParser = require('fast-xml-parser');
+
+    const xmlRawPayload = fs.readFileSync(`${__dirname}/../payload.xml`).toString('utf8');
+    const options = {
+        attrNodeName: 'attr',
+        ignoreAttributes : false,
+    };
+
+    for (let i = 0; i < iterationCount; i++) {
+        xmlParser.parse(xmlRawPayload, options);
+    }
+
+    process.exit(0);
+}
